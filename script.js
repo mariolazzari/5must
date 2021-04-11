2814d16d7d21e46436b4102f6dae8a118ca39ac3
Mario Lazzari <mario.lazzari@gmail.com>
2021-04-11 14:15:38 +0200

    Done lesson 2

--- script.js
+++ script.js
@@ -57,3 +57,119 @@ console.log("miss 1st", firstHobby);
 // script defer attribute
 const button = document.querySelector("button");
 button.style.backgroundColor = "green";
+
+// scope
+const a = 1;
+
+function main() {
+  const a = 2;
+  console.log(`In main: ${a}`);
+}
+
+{
+  const a = 3;
+  console.log(`In brackets: ${a}`);
+}
+
+main();
+console.log(`In global: ${a}`);
+
+// scope in switch
+switch (a) {
+  case 1:
+    {
+      const result = a * 2;
+      console.log(result);
+    }
+    break;
+
+  case 2:
+    {
+      const result = a + 2;
+      console.log(result);
+    }
+    break;
+
+  case 3:
+    {
+      const result = a - 2;
+      console.log(result);
+    }
+    break;
+}
+
+// in keyword
+const person = {
+  name: "Kyle",
+  age: 22,
+};
+
+if (person.name) {
+  console.log("person has name");
+}
+
+// don't care about value (undefined, '')
+if ("name" in person) {
+  console.log("person has name");
+}
+
+// tagged template litterals: https://blog.webdevsimplified.com/2020-03/tagged-template-literals/
+function custom(strings, ...values) {
+  console.log(strings, values);
+  return "hi";
+}
+
+const firstName = "Mario";
+const hobby = "coding";
+console.log(custom`My name is ${firstName} and I love ${hobby}.`);
+
+// generators: https://blog.webdevsimplified.com/2021-01/javascript-generators/
+function* generatorFunction() {
+  console.log("before 1");
+  yield 1;
+  console.log("after 1");
+  console.log("before 2");
+  yield 2;
+  console.log("after 2");
+  console.log("before 3");
+  yield 3;
+  console.log("after 3");
+}
+
+const generator = generatorFunction();
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+
+function* idGenerator() {
+  let id = 1;
+
+  while (true) {
+    yield id;
+    id++;
+  }
+}
+
+const genId = idGenerator();
+
+const person1 = {
+  id: genId.next().value,
+  name: "Person One",
+};
+
+const person2 = {
+  id: genId.next().value,
+  name: "Person Two",
+};
+
+const person3 = {
+  id: genId.next().value,
+  name: "Person Three",
+};
+
+console.log("Generators:", person1);
+console.log("Generators:", person2);
+console.log("Generators:", person3);
+
+// dynamic imports: https://www.youtube.com/watch?v=cRHQNNcYf6s
